49c44df9b783b32bc6f89d039df95cd0cbb0808d
Patrick Patenaude <ppatenaude@hotmail.fr>
2025-08-25 21:30:45 -0400

    reporter: inline assets and embed screenshots to avoid blank report pages in artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    charts: true,
+    reportPageTitle: 'AutomationExercise E2E Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
